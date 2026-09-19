061885da4d225156277a031f2e7a38955a70c616
M. sumithra <sumithramuthumuthu2006@gmail.com>
2026-09-19 19:27:41 +0530

    Add files via upload

---
+++ if,else,elif.py
@@ -0,0 +1,38 @@
+if statement:
+a=100
+if a==10:
+    print("the give number is equal to 100")
+else:
+    print("the given number is not equal to 100")
+#if and elif statement
+a=75
+if a>=80:
+    print("grade A")
+elif a>=60:
+    print("grade B")
+else:
+    print("grade c")
+#else statement
+num=5
+if num>0:
+    print("positive number")
+else:
+    print("negative number")
+#nested if else statement
+age=18
+ID=True
+if age>=18:
+    if id:
+        print("allwoed")
+    else:
+        print("not allwoed")
+else:
+    print("not eligbil")
+#elif and if condition
+num=20
+if num>=0:
+    print("positive number")
+elif num<=0:
+    print("negative number")
+else:
+    print("zero")
