ce5e3cc2d1de191aa44bd29a15e9d34da288a895
M. sumithra <sumithramuthumuthu2006@gmail.com>
2026-09-19 20:00:16 +0530

    Add files via upload

---
+++ string and slicing.py
@@ -0,0 +1,25 @@
+#string
+'''a="student"
+print(a.capitalize())
+print(a.lower())
+print(a.upper())
+print(a.isalpha())
+print(a.isdigit())
+print(a.isalnum())
+print(len(a))
+print(a.count("t"))
+print(a.index("e"))
+print(a.split("d"))
+print(a.isascii())
+print(a[5])
+print(a[1:5])
+print(a.center(1,"*"))'''
+
+#slicing model
+a = "sumithrakarthi"
+'''print(a[0:4])
+print(a[0:11])
+print(a[0:14])
+print(a[::-1])
+print(a[:-1])
+print(a[1::2])'''
